fca9478bb5001f77325851f198a4f1ea5fa1439e
unknown <you@example.com>
2024-03-01 15:15:15 +0700

    done

--- src/utils/data.js
+++ src/utils/data.js
@@ -53,7 +53,6 @@ export const skills = {
         },
         {
             title: 'Tailwind CSS',
-            // logo: 'https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png'
             logo: 'https://static-00.iconduck.com/assets.00/tailwind-css-icon-2048x1229-u8dzt4uh.png'
         },
     ]
@@ -62,15 +61,15 @@ export const skills = {
 export const projects = {
     allProject: [
         {
-            image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_A88XPFHokXRDEfA8hW22T3za6WAK%26teamId%3Dtien-dats-projects-817f1529%26withStatus%3D1&w=640&q=75&dpl=dpl_Bg4SViBcuwpGGQEBd47FhcXUp7s1',
+            image: 'https://image.tmdb.org/t/p/w500//h3jYanWMEJq6JJsCopy1h7cT2Hs.jpg',
             title: 'Movie App',
             description: 'Responsive React Movies App With API',
             sourcecode: 'https://github.com/tiendat3m/d-movie',
             livelink: 'https://d-movie.vercel.app/',
-            teckstack: ["React", "Scss"]
+            teckstack: ["React", "Sass"]
         },
         {
-            image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_CbfNJiQFTcLLXg38YKb8KP2vtyzH%26teamId%3Dtien-dats-projects-817f1529%26withStatus%3D1&w=640&q=75&dpl=dpl_Bg4SViBcuwpGGQEBd47FhcXUp7s1',
+            image: 'https://photo-zmp3.zmdcdn.me/banner/2/e/c/1/2ec1e2bacee5bf84e3bc5322ac2cf764.jpg',
             title: 'Music App| ZingMp3',
             description: 'A music web application like ZingMp3 where user can search and listen their favourite songs, artist',
             sourcecode: 'https://github.com/tiendat3m/clone-zingmp3',
@@ -85,108 +84,6 @@ export const projects = {
             livelink: 'https://tiendat3m.github.io/weather-app/',
             teckstack: ["HTML", "CSS", "Javascript"]
         },
-        //         {
-        //             image: 'https://img.freepik.com/premium-vector/man-with-magnifying-glass-travel-book-tickets-hotel-room-apartment-reservation-service-transfer-transportation-traveling_48369-37531.jpg?w=360',
-        //             title: 'Make My Booking | Live Site N/A',
-        //             description: 'A simple mobile App in which users can see hotels by location and they can book rooms in hotels',
-        //             sourcecode: 'https://github.com/piyush362/page-not-found',
-        //             livelink: '',
-        //             teckstack: ["React", "React Native", "Node.js"]
-        //         },
-        //         {
-        //             image: 'https://static.vecteezy.com/system/resources/thumbnails/012/494/550/small/inventory-control-system-concept-professional-manager-and-worker-are-checking-goods-and-stock-supply-inventory-management-with-goods-demand-vector.jpg',
-        //             title: 'INVENTORY MANAGEMENT SYSTEM | LIVE site available',
-        //             description: 'Inventory management system, able to perform CRUD operation to manage stocks',
-        //             sourcecode: 'https://docs.google.com/document/d/1GIpTldeZ7guKhRI6tvyfHzi7PbA8Mx-qteum3VdiB8U/edit?usp=sharing',
-        //             livelink: 'https://myinventorysys.netlify.app/',
-        //             teckstack: ["ReactJs", "Node.js", "mongoDB", "ExpressJs"]
-        //         },
-        //         {
-        //             image: 'https://img.freepik.com/free-vector/realistic-horizontal-cinema-movie-time-poster-with-3d-glasses-snacks-tickets-clapper-reel-blue-background-with-bokeh-vector-illustration_1284-77013.jpg',
-        //             title: 'MOVIE WORLD- Movies search | LIVE',
-        //             description: 'A simple React Page that fetch movies details from API and show it on card, users can search movies too.',
-        //             sourcecode: 'https://github.com/piyush362/movie-search',
-        //             livelink: 'https://movies-search23.netlify.app/',
-        //             teckstack: ["ReactJs", "REST API", "Axios"]
-        //         },
-        //         {
-        //             image: 'https://res-2.cloudinary.com/under-the-hood-learning/image/upload/q_auto/v1/blog_images/MVC-Vanilla-JS.png',
-        //             title: 'VANILLA JS PROJECTS | LIVE',
-        //             description: 'Few project (Rock Paper Scissors, Tip calculator, Color Flipper) that is built using vanilla js ( i.e: without any javascript framework)',
-        //             sourcecode: 'https://github.com/piyush362/Basic-JavaScript-Projects',
-        //             livelink: 'https://piyush362.github.io/Basic-JavaScript-Projects/',
-        //             teckstack: ["JavaScript"]
-        //         },
-        //     ],
-        //     uiProject: [
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         }
-        //     ],
-        //     reactProject: [
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         }
-
-        //     ],
-        //     reactNativeProject: [
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Social Birds',
-        //             description: 'A social media application ',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         },
-        //         {
-        //             image: '/assests/images/project.png',
-        //             title: 'Some Title',
-        //             description: '',
-        //             sourcecode: '',
-        //             livelink: ''
-        //         }
 
     ]
 }
